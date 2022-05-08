1b08cf8a2ffb5420827b74dabd54f3926d2b46a5
ShunKato0625 <shunkato0625@gmail.com>
2022-05-08 18:10:58 +0000

    [Update] routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,38 @@
 Rails.application.routes.draw do
-# 顧客用
-devise_for :customers,skip: [:passwords],controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  scope module: :public do
+    resource :customers, only: [:edit, :update] do
+      get 'my_page' => 'customers#show'
+      get 'unsubscribe' => 'customers#unsubscribe'
+      patch 'withdraw' => 'customers#withdraw'
+    end
+  end
+  # 顧客用
+  devise_for :customers,skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
-# 管理者用
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  namespace :admin do
+    root 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update] do
+      get 'post_index' => 'customers#post_index'
+    end
+    resources :items, only: [:index, :show, :edit, :update]
+    get 'search'=> 'searches#search'
+  end
 
-
-  get 'homes/top'
+  scope module: :public do
+    root 'homes#top'
+    get 'homes/about'
+    resources :posts do
+      resources :post_comments, only: [:index, :create]
+      resource :favorites, only: [:create, :destroy]
+    end
+    get 'search' => 'searches#search'
+  end
   
 end
