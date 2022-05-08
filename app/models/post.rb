6fbda1a462d579a4d4c822f4a88719594b1d70f5
ShunKato0625 <shunkato0625@gmail.com>
2022-05-08 17:38:40 +0000

    [Update] post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,35 @@
 class Post < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  
-  belongs_to :customer
-  belongs_to :post
 
-  validates_uniqueness_of :post_id, scope: :customer_id
+  belongs_to :customer
+  has_many :favorites          , dependent: :destroy
+  has_many :post_comments      , dependent: :destroy
+  has_many :favorited_customers, through: :favorites,
+                                 source: :customer
+
+  validates :title             , presence: true
+  validates :remarks           , presence: true,
+                                 length: { maximum: 300 }
+  validates :access            , presence: true,
+                                 length: { maximum: 200 }
+  validates :authorization     , presence: true,
+                                 length: { maximum: 200 }
+  validates :drone_type        , presence: true
+  validates :rate              , numericality: {
+                                 less_than_or_equal_to: 5,
+                                 greater_than_or_equal_to: 1,
+                                 }, presence: true
+  validates :shooting_date     , presence: true
+
+
+
+
+  def favorited_by?(customer)
+    favorites.exists?(customer_id: customer.id)
+  end
+
+
+  is_impressionable counter_cache: true
+
 end
