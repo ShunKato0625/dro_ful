1cbf66d531fea9aa7e75fb117be9c675af5779e1
ShunKato0625 <shunkato0625@gmail.com>
2022-05-08 17:21:05 +0000

    [Update] favorite model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  
+  belongs_to :customer
+  belongs_to :post
+
+  validates_uniqueness_of :post_id, scope: :customer_id
 end
