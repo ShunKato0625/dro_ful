f80bc792a6331ae66e50a2c97c95963cc4500414
ShunKato0625 <shunkato0625@gmail.com>
2022-05-12 18:10:56 +0000

    [Update] post mypage layout

--- app/models/post_request.rb
+++
@@ -1,4 +0,0 @@
-# class PostRequest < ApplicationRecord
-#   has_many_active_hash :requests
-#   belongs_to :post
-# end
--- db/migrate/20220512114331_create_post_requests.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostRequests < ActiveRecord::Migration[6.1]
-  def change
-    create_table :post_requests do |t|
-      t.integer      :post_id
-      t.integer      :request_id
-      t.timestamps
-    end
-  end
-end
