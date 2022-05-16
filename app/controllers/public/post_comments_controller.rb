e75db607800c9f40b1accbe269f59b29dd410174
ShunKato0625 <shunkato0625@gmail.com>
2022-05-16 12:46:36 +0000

    [Update]コメント、いいね非同期完了

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,19 +3,17 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    @customer = current_customer
     @post_comment = current_customer.post_comments.new(post_comment_params)
     @post_comment.post_id = @post.id
-    flash.now[:notice] = "コメントを投稿しました"
     render :validater unless @post_comment.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    @customer = current_customer
     @post_comment = @post.post_comments.find(params[:id])
     @post_comment.destroy
-    # PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    flash[:alert] = '投稿を削除しました'
-    # @post = Post.find(params[:post_id])
   end
 
   private
