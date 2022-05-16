22f34df0a7c43d6adf97a466b28a033bd92b55aa
ShunKato0625 <shunkato0625@gmail.com>
2022-05-16 08:54:57 +0000

    [Update] admin search view

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -4,12 +4,12 @@ class Admin::SearchesController < ApplicationController
   def search_post
     # @content = params[:content]
     # @records = Post.where(is_active: 1).search_for(@content).page(params[:page]).per(6)
-    @posts = Post.search_post(params[:keyword])
+    @posts = Post.search_post(params[:keyword]).page(params[:page]).per(10)
   end
 
   def search_customer
     # @content = params[:content]
     # @records = Post.where(is_active: 1).search_for(@content).page(params[:page]).per(6)
-    @customers = Customer.search_customer(params[:keyword])
+    @customers = Customer.search_customer(params[:keyword]).page(params[:page]).per(10)
   end
 end
