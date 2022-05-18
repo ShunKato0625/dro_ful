e4691e790b5ad232b81420f9d5b82f6edb26da90
ShunKato0625 <shunkato0625@gmail.com>
2022-05-18 16:54:10 +0000

    [Update] guest index show

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,5 @@
 class Public::PostsController < ApplicationController
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, except: [:show, :index]
   impressionist :actions => [:show]
 
   def new
