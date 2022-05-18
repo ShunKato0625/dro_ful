dd33de17dad50e2a9ed46483e0d08269c5229c1b
ShunKato0625 <shunkato0625@gmail.com>
2022-05-18 01:35:53 +0000

    [Update] プロフィール画像編集できないのを修正

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -28,6 +28,6 @@ class Admin::CustomersController < ApplicationController
 
     def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
-                                       :nick_name, :email, :is_deleted)
+                                       :nick_name, :email, :is_deleted, :profile_image)
     end
 end
